483233a1a7b2f3b72a8d835bffa0c77d942aa71e
Saidakbar2009 <ituchun18@gmail.com>
2023-10-31 20:39:02 +0500

    admin panelda yangilanishlar bor

--- app1/admin.py
+++ app1/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import *
-admin.site.register(Yonalish)
-admin.site.register(Fan)
-admin.site.register(Ustoz)
+from django.contrib.auth.models import *
+# Register your models here.
+@admin.register(Ustoz)
+class UstozAdmin(admin.ModelAdmin):
+    list_display = ['ism', 'jins', 'daraja', 'yosh', 'fan']
+    search_fields = ['ism']
+
+@admin.register(Yonalish)
+class YonalishAdmin(admin.ModelAdmin):
+    list_display = ['nom', 'aktiv']
+    search_fields = ['nom']
+    list_filter = ['aktiv']
+
+@admin.register(Fan)
+class FanAdmin(admin.ModelAdmin):
+    list_display = ['nom', 'yonalish', 'asosiy']
+    list_filter = ['asosiy']
+    search_fields = ['nom']
+# admin.site.register(Yonalish)
+# admin.site.register(Fan)
+# admin.site.register(Ustoz)
